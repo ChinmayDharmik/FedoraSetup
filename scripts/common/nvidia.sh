59aa0b03fd51f469f7dd039169f7ccc76f3023ff
Chinmay Dharmik <94623147+ChinmayDharmik@users.noreply.github.com>
2025-03-08 19:39:23 +1030

    Update nvidia.sh

--- scripts/common/nvidia.sh
+++ scripts/common/nvidia.sh
@@ -6,15 +6,19 @@ sudo kmodgenca -a
 
 sudo mokutil --import /etc/pki/akmods/certs/public_key.der
 
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs -y
 
 sudo akmods --force
 sudo dracut --force
 
-sudo dnf config-manager add-repo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/fedora39/x86_64/cuda-fedora39.repo
-sudo dnf clean all -y
+sudo dnf -y install nvidia-open
+sudo dnf -y install cuda-drivers
+
 sudo dnf module disable nvidia-driver
+sudo dnf config-manager addrepo --from-repofile https://developer.download.nvidia.com/compute/cuda/repos/fedora41/x86_64/cuda-fedora41.repo
+sudo dnf clean all
+sudo dnf -y install cuda-toolkit-12-8
 sudo dnf -y install cuda
 
-sudo dnf install https://developer.download.nvidia.com/compute/machine-learning/repos/rhel8/x86_64/nvidia-machine-learning-repo-rhel8-1.0.0-1.x86_64.rpm
+sudo dnf install https://developer.download.nvidia.com/compute/machine-learning/repos/rhel8/x86_64/nvidia-machine-learning-repo-rhel8-1.0.0-1.x86_64.rpm -y
 sudo dnf install libcudnn7 libcudnn7-devel libnccl libnccl-devel -y
